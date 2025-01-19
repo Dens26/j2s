bbb1146d672f717aed22cef6f7cb31ed37492a6c
Dens26 <denis.bekaert@live.fr>
2025-01-19 23:51:35 +0100

    👌 IMPROVE: game restriction

--- src/Controller/Admin/GameCrudController.php
+++ src/Controller/Admin/GameCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Game;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +19,39 @@ class GameCrudController extends AbstractCrudController
         return Game::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->setPermission(Action::EDIT, 'ROLE_ADMIN')   // Les admins peuvent éditer
+        ->setPermission(Action::NEW, 'ROLE_SUPER_ADMIN')             // Interdire la création
+        ->setPermission(Action::DELETE, 'ROLE_SUPER_ADMIN')          // Interdire la suppression
+    ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            NumberField::new('gameId')->setLabel('Id')->onlyOnIndex(),
+            NumberField::new('yearPublished')->setLabel('Année de publication'),
+            TextField::new('name')->setLabel('Nom'),
+            TextField::new('allNames')->setLabel('Autres noms'),
+            NumberField::new('minPlayers')->setLabel('Joueur min.'),
+            NumberField::new('maxPlayers')->setLabel('Joueur max.'),
+            NumberField::new('playingTime')->setLabel('Temps de jeu'),
+            NumberField::new('age')->setLabel('Age'),
+            TextareaField::new('description')->setLabel('Description'),
+            
+            /*
+            allNames
+            year_published
+            min_players
+            max_players
+            playing_time
+            age
+            description
+            thumbnail
+            image
+            */
         ];
     }
-    */
 }
